43fe89ed8f9e7bf31a8ea416a6c907edeac0ee2d
mohamed-jebali <mohamedjebali992010@gmail.com>
2023-05-18 17:52:54 +0200

    added snack4

--- jsnack4/js/script.js
+++ jsnack4/js/script.js
@@ -1,5 +1,6 @@
 let inputUser = parseInt(prompt("Inserisci un Numero con massimo di 4 cifre"));
 let tot = 0;
+let inputNumber = inputUser.toString();
 
 if(isNaN(inputUser)){
     console.log("Inserisci un numero valido");
@@ -9,5 +10,10 @@ else if(inputUser.toString().length > 4){
     console.log("Errore : hai inserito un numero con piu di 4 cifre")
 }
 else{
-    console.log("la somma dei numeri inseriti e : " + inputUser.length + tot)
+    for(let i = 0; i <inputNumber.length;i++){
+
+        tot += parseInt(inputNumber[i]);
+
+    }
+    console.log("la somma dei numeri inseriti e : " , tot);
 }
