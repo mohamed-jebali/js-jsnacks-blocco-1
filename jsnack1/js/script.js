81ec05ca5288625eb3ebf91f6178c4dca47ad6d3
mohamed-jebali <mohamedjebali992010@gmail.com>
2023-05-18 15:21:28 +0200

    working on snack1

---
+++ jsnack1/js/script.js
@@ -0,0 +1,10 @@
+
+
+for (let i = 0; i < 10; i++) {
+    let inputUser = prompt("Inserisci Numero " + i);
+    console.log("Risposta alla domanda " + i + inputUser);
+  }
+  
+  if(inputUser){
+    console.log(inputUser + i);
+  }
